20744fc2c18c41dcd5bdcaa39d917623fe863e8f
sayedshaun <sayedshaun4@gmail.com>
2026-05-03 09:53:51 +0600

    fix: correct import statement for BrowserSettings in __init__.py

--- onecrawler/__init__.py
+++ onecrawler/__init__.py
@@ -1,4 +1,5 @@
 from .config.crawler import CrawlerSettings
+from .config.brawser import BrowserSettings
 from .crawler.link.engine import LinkExtractionEngine
 from .crawler.scraper.engine import ScraperEngine
 from .map.sitemap import SiteMap, Stats
