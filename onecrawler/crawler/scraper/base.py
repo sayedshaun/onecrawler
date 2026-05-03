fc8254a74a904af37deda6b1f4de99671dc8bcbc
sayedshaun <sayedshaun4@gmail.com>
2026-05-03 22:55:05 +0600

    feat: add abstract context management methods to BaseStrategy

--- onecrawler/crawler/scraper/base.py
+++ onecrawler/crawler/scraper/base.py
@@ -4,5 +4,13 @@ from typing import Any
 
 class BaseStrategy(ABC):
     @abstractmethod
+    async def __aenter__(self) -> Any:
+        pass
+
+    @abstractmethod
+    async def __aexit__(self, exc_type, exc, tb) -> None:
+        pass
+
+    @abstractmethod
     async def extract(self, url: str) -> Any:
         pass
