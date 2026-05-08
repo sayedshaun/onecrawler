b340f13d139476d1b98056f7d14c377917e0898e
sayedshaun <sayedshaun4@gmail.com>
2026-05-08 10:47:49 +0600

    fixed formatting

--- onecrawler/map/sitemap.py
+++ onecrawler/map/sitemap.py
@@ -517,7 +517,9 @@ class UniversalSiteMap:
             all_records = [
                 r
                 for r in all_records
-                if wildcard_link_match(r.url, base_url, self.config.include_link_patterns)
+                if wildcard_link_match(
+                    r.url, base_url, self.config.include_link_patterns
+                )
             ]
 
         # Deduplication
