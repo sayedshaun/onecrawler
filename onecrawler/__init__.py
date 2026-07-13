526fe43f785b138fc9c410fb3e96d36194cdb2bb
Sayed Shaun <sayedshaun4@gmail.com>
2026-07-13 21:32:55 +0600

    refactor: export new enums from top-level package

--- onecrawler/__init__.py
+++ onecrawler/__init__.py
@@ -2,9 +2,14 @@ from .crawler import Crawler, LinkExtractor, Scraper
 from .crawler.map.sitemap import SiteMap, SitemapStats, UniversalSiteMap
 from .settings import (
     BrowserSettings,
+    GenAIProvider,
     GenerativeAISettings,
     HumanBehaviorSettings,
+    LinkExtractionStrategy,
+    OutputFormat,
+    ProxyRotationMethod,
     ProxySettings,
+    ScrapingStrategy,
     Settings,
 )
 from .version import __version__
